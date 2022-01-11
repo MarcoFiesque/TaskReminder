0f414e5f8e8b22d6216e3e0b42d2f79702a3e7b7
Marcos <marco.fiesque@gmail.com>
2022-01-11 09:49:37 +0100

    On progress

---
+++ TaskReminder/Server/Controllers/ApiController.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskReminder.Server.Services;
+using TaskReminder.Shared;
+
+namespace TaskReminder.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private TodoService _todoService;
+
+        public ApiController(TodoService todoService)
+        {
+            _todoService = todoService;
+        }
+
+        // GET: api/TodoApi
+        [HttpGet]
+        public ActionResult<List<TodoItem>> GetTodos()
+        {
+            return _todoService.GetTodos();
+        }
+
+        // GET: api/TodoApi/5
+        [HttpGet("{id:length(24)}", Name = "Get")]
+        public ActionResult<TodoItem> GetTodoById(string id)
+        {
+            return _todoService.GetTodo(id);
+        }
+
+        // POST: api/TodoApi
+        [HttpPost]
+        public TodoItem Post(TodoItem todo)
+        {
+            _todoService.PostTodo(todo);
+            return todo;
+        }
+
+        // PUT: api/TodoApi/5
+        [HttpPut("{id:length(24)}")]
+        public TodoItem Put(string id, TodoItem newTodo)
+        {
+            return _todoService.PutTodo(id, newTodo);
+
+        }
+
+        // DELETE: api/TodoApi/5
+        [HttpDelete("{id:length(24)}")]
+        public TodoItem Delete(string id)
+        {
+           return _todoService.DeleteTodo(id);
+        }
+    }
+}
---
+++ TaskReminder/Shared/TodoItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TaskReminder.Shared
+{
+    public class TodoItem
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement]
+        public string Title { get; set; }
+        [BsonElement]
+        public string? Description { get; set; }
+        [BsonElement]
+        public bool IsDone { get; set; }
+
+        public TodoItem()
+        {
+        }
+
+    }
+}
